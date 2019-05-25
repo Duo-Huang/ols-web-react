eaf63187197de41d0257361a078990b9e0c82dd2
myh_cs <myh_cs@myh-csdeMacBook-Pro.local>
2019-05-25 20:37:56 +0800

    mock 数据及实现

--- server/mocker/index.js
+++ server/mocker/index.js
@@ -1,5 +1,5 @@
 const proxy = {
-    'GET /api/user': {id: 1, username: 'kenny', sex: 6 },
+    'GET /api/user': { id: 1, username: 'kenny', sex: 6 },
     'POST /api/login': (req, res) => {
         const { password, username } = req.body;
         if (password === 'admin888' && username === 'admin') {
@@ -7,11 +7,18 @@ const proxy = {
                 status: 'ok',
                 code: 200,
                 token: "thisistoken",
-                data: {id: 1, username: 'kenny', sex: 6 }
+                data: { id: 1, username: 'kenny', sex: 6 }
             });
         } else {
-            return res.send({status: 'error', code: 403 });
+            return res.send({ status: 'error', code: 403 });
         }
     },
+    'GET /trainingcamps/%7Bid%7D': {
+        "id": 1,
+        "title": "xx训练营",
+        "start_time": "2018-01-02 10:12:55.222",
+        "description": "This is a training camp",
+        "created_time": "2018-01-01 09:12:33.443"
+    }
 }
 module.exports = proxy;
