682c9a7086cae507230cec350f4117d1ee6a1cca
Ming Ma <ming.ma@CNming.ma>
2019-05-25 16:16:07 +0800

    router

---
+++ src/containers/Create/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default class Create extends React.Component {
+  render() {
+    return (
+      <div>
+        创建
+      </div>
+    )
+  }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { BrowserRouter,Route } from 'react-router-dom'
 import Login from '../components/Login'
 import Logup from '../components/Logup'
-
+import Create from '../containers/Create';
 const Router = () => (
     <BrowserRouter>
         <div>
             <Route exact path="/" component={Login}/>
             <Route path="/log-up" component={Logup}/>
+            {/* <Route path="/list" component={Logup}/> */}
+            {/* <Route path="/detail" component={Logup}/> */}
+            <Route path="/create" component={Create}/>
         </div>
     </BrowserRouter>
 );
